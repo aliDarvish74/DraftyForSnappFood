6cf6bc4a4397ce5556d86e1e18d18755d4b9f347
Ali Darvish <109904348+aliDarvish74@users.noreply.github.com>
2022-08-13 15:21:24 +0430

    Update doneScript.js

---
+++ done/doneScript.js
@@ -0,0 +1,45 @@
+function itemCont1() {
+  navigator.clipboard.writeText("مغایرت سفارش\nاز وندور 1 امتیاز کسر شد");
+  alert("پیش نویس شما کپی شد!");
+}
+function itemCont2() {
+  navigator.clipboard.writeText("مغایرت سفارش\nاز وندور 2 امتیاز کسر شد");
+  alert("پیش نویس شما کپی شد!");
+}
+
+function venDelay() {
+  navigator.clipboard.writeText("تاخیر وندور\nاز وندور 3 امتیاز کسر شد");
+  alert("پیش نویس شما کپی شد!");
+}
+
+function hygiene() {
+  navigator.clipboard.writeText("عدم رعایت بهداشت\nاز وندور 5 امتیاز کسر شد");
+  alert("پیش نویس شما کپی شد!");
+}
+
+function pack() {
+  navigator.clipboard.writeText("بسته بندی نامناسب\nاز وندور 2 امتیاز کسر شد");
+  alert("پیش نویس شما کپی شد!");
+}
+
+function description() {
+  navigator.clipboard.writeText(
+    "عدم توجه به توضیحات\nاز وندور 1 امتیاز کسر شد"
+  );
+  alert("پیش نویس شما کپی شد!");
+}
+
+function notDel() {
+  navigator.clipboard.writeText("عدم تحویل سفارش\nاز وندور 3 امتیاز کسر شد");
+  alert("پیش نویس شما کپی شد!");
+}
+
+function badDel() {
+  navigator.clipboard.writeText("حمل نامناسب\nاز وندور 2 امتیاز کسر شد");
+  alert("پیش نویس شما کپی شد!");
+}
+
+function imgCont() {
+  navigator.clipboard.writeText("مغایرت تصویر منو\nاز وندور 2 امتیاز کسر شد");
+  alert("پیش نویس شما کپی شد!");
+}
